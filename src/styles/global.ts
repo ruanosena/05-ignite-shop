7ddad41f8c482e1168085580ba118ebffe6c92f7
ruanosena <ruosena@hotmail.com>
2024-02-04 20:13:15 -0300

    estrutura visual

---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from ".";
+
+export const globalStyles = globalCss({
+	"*": {
+		margin: 0,
+		padding: 0,
+	},
+	body: {
+		backgroundColor: "$gray900",
+		color: "$gray100",
+		"font-synthesis": "none",
+		"text-rendering": "optimizeLegibility",
+		"-webkit-font-smoothing": "antialiased,",
+		"-moz-osx-font-smoothing": "grayscale",
+	},
+	"body, input, textarea, button": {
+		fontFamily: "Roboto",
+		fontWeight: 400,
+	},
+});
